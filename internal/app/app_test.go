35fbada6c43249598412f30e94e7e9ae3fc1ea35
Uchencho <aloziekelechi17@gmail.com>
2021-11-06 20:56:12 +0100

    interface implementation and test

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -143,3 +143,123 @@ func TestCreateUser(t *testing.T) {
 		getUserInvoked, insertUserInvoked = false, false
 	}
 }
+
+func TestCreateUserV2(t *testing.T) {
+
+	var (
+		getUserInvoked, insertUserInvoked bool
+	)
+
+	table := []row{
+		{
+			name: "successfully create user",
+			in:   pkg.CreateUserRequest{Email: "uche@gmail.com", Name: "uche", Password: "testPass"},
+			out:  http.StatusNoContent,
+			mocks: func(oa *app.OptionalArgs) {
+				oa.GetUser = func(email string) (internal.User, error) {
+					getUserInvoked = true
+					return internal.User{}, errors.Errorf("user does not exist")
+				}
+
+				oa.HashPassword = func(password string) string {
+					return "hashed-password"
+				}
+
+				oa.InsertUser = func(u internal.User) error {
+					insertUserInvoked = true
+					return nil
+				}
+			},
+			customTest: func(resp *http.Response, name string) {
+				t.Run(fmt.Sprintf("%s-http response is as expected", name), func(t *testing.T) {
+					assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+				})
+				t.Run(fmt.Sprintf("%s-get user is invoked", name), func(t *testing.T) {
+					assert.True(t, getUserInvoked)
+				})
+				t.Run(fmt.Sprintf("%s-insert user is invoked", name), func(t *testing.T) {
+					assert.True(t, insertUserInvoked)
+				})
+			},
+		},
+		{
+			name: "fails to create user due to user already existing",
+			in:   pkg.CreateUserRequest{Email: "uche@gmail.com", Name: "uche", Password: "testPass"},
+			out:  http.StatusBadRequest,
+			mocks: func(oa *app.OptionalArgs) {
+				oa.GetUser = func(email string) (internal.User, error) {
+					getUserInvoked = true
+					return internal.User{}, nil
+				}
+
+				oa.HashPassword = func(password string) string {
+					return "hashed-password"
+				}
+
+				oa.InsertUser = func(u internal.User) error {
+					insertUserInvoked = true
+					return nil
+				}
+			},
+			customTest: func(resp *http.Response, name string) {
+				t.Run(fmt.Sprintf("%s-http response is as expected", name), func(t *testing.T) {
+					assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+				})
+				t.Run(fmt.Sprintf("%s-get user is invoked", name), func(t *testing.T) {
+					assert.True(t, getUserInvoked)
+				})
+				t.Run(fmt.Sprintf("%s-insert user is NOT invoked", name), func(t *testing.T) {
+					assert.False(t, insertUserInvoked)
+				})
+			},
+		},
+		{
+			name: "fails to create user due to invalid payload",
+			in:   pkg.CreateUserRequest{Email: "uche@gmail.com"},
+			out:  http.StatusBadRequest,
+			mocks: func(oa *app.OptionalArgs) {
+				oa.GetUser = func(email string) (internal.User, error) {
+					getUserInvoked = true
+					return internal.User{}, nil
+				}
+
+				oa.HashPassword = func(password string) string {
+					return "hashed-password"
+				}
+
+				oa.InsertUser = func(u internal.User) error {
+					insertUserInvoked = true
+					return nil
+				}
+			},
+			customTest: func(resp *http.Response, name string) {
+				t.Run(fmt.Sprintf("%s-http response is as expected", name), func(t *testing.T) {
+					assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+				})
+				t.Run(fmt.Sprintf("%s-get user is NOT invoked", name), func(t *testing.T) {
+					assert.False(t, getUserInvoked)
+				})
+				t.Run(fmt.Sprintf("%s-insert user is NOT invoked", name), func(t *testing.T) {
+					assert.False(t, insertUserInvoked)
+				})
+			},
+		},
+	}
+
+	for _, entry := range table {
+		a := app.New(entry.mocks)
+		ht := httptest.NewServer(a.Handler())
+		defer ht.Close()
+
+		log.Printf("\n\n\nRunning Test : %s \n\n\n", entry.name)
+
+		bb, _ := json.Marshal(entry.in)
+		rr := bytes.NewReader(bb)
+		req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/v2/user", ht.URL), rr)
+		resp, _ := http.DefaultClient.Do(req)
+
+		entry.customTest(resp, entry.name)
+
+		getUserInvoked, insertUserInvoked = false, false
+	}
+}
